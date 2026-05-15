465d7bdc1e0341a8ccc1876bfcf3ca8f198ecf1d
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-05-15 22:38:00 +0600

    signup successfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,22 @@
 const { isValidEmail } = require("../helpers/utils");
+const userSchema = require("../models/userSchema");
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
   const { fullname, email, password } = req.body;
 
   try {
     if (!fullname) return res.status(400).send("Fullname is required");
+    if (!email) return res.status(400).send("Email is required");
     if (!isValidEmail(email)) return res.status(400).send("Invalid email");
     if (!password) return res.status(400).send("Password is required");
+
+    // ---------exesting email check 
+    const existingUser = await userSchema.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send("User with this email already exists");
+    }
   } catch (err) {
-    console.error("Error during signup:", err);
-    return res.status(500).send("An error occurred during signup");
+    return res.status(500).send("Server error");
   }
 
   res.status(200).send("Signup successful!");
