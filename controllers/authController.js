d6350e0b5081a554e1026ff4db0cb352af794b59
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-05-15 22:24:09 +0600

    utils add

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,20 @@
+const { isValidEmail } = require("../helpers/utils");
+
 const signup = (req, res) => {
+  const { fullname, email, password } = req.body;
+
+  try {
+    if (!fullname) return res.status(400).send("Fullname is required");
+    if (!isValidEmail(email)) return res.status(400).send("Invalid email");
+    if (!password) return res.status(400).send("Password is required");
+  } catch (err) {
+    console.error("Error during signup:", err);
+    return res.status(500).send("An error occurred during signup");
+  }
+
   res.status(200).send("Signup successful!");
 };
 
-
-
 module.exports = {
-  signup
+  signup,
 };
