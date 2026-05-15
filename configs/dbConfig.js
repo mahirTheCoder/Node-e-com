6416c4b738567acba4db4f5a0c687c3d4fa83016
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-05-15 21:22:37 +0600

    db configs add

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 
-const dbConfig = async () => {
- const url = process.env.MONGODB_URL;
- if(!url) {
-  console.error("MONGODB_URL is not defined in environment variables"); }
+const dbConfig = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Mongodb Connected successfully!"));
 };
-
-
-try{
-  await mongoose.connect(url);
-  console.log("Connected to MongoDB successfully");
-} catch (error) {
-  console.error("Error connecting to MongoDB:", error);
-}
 
 module.exports = dbConfig
